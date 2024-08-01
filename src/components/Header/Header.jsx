ce3d598115890429ee26ef14f40103add67849f0
Arif Hossain <arif.raj9911@gmail.com>
2024-08-02 01:26:49 +0600

    side navbar design implemented

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,84 @@
-
+import { Link } from "react-router-dom";
+import homeIcon from "../../assets/icons/home.png";
+import userIcon from "../../assets/icons/user.png";
+import searchIcon from "../../assets/icons/search.png";
+import aboutIcon from "../../assets/icons/Iconly_Light_Paper.png";
+import favoriteIcon from "../../assets/icons/favorite.png";
+import infoIcon from "../../assets/icons/info.png";
+import settingsIcon from "../../assets/icons/Setting.png";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>This is a Navbar</h2>
-        </div>
-    );
+  return (
+    <div className="py-12 ">
+      <h2 className="text-[40px] mb-10 pl-28 text-[#4285F3]">LOGO</h2>
+      <ul className="flex flex-col   items-start text-[#5C635A]">
+        <li className="mb-2  w-full flex pl-16 py-2  hover:bg-[#D5E9FF] hover:text-black text-lg  text-left ">
+          <Link className="flex gap-x-4 items-center justify-center" to="">
+            <span>
+              {" "}
+              <img src={homeIcon} alt="home" />
+            </span>
+            <span>Home</span>
+          </Link>
+        </li>
+        <li className="mb-2  w-full flex pl-16 py-2  hover:bg-[#D5E9FF] hover:text-black  text-lg   text-left">
+          <Link className="flex gap-x-4 items-center justify-center" to="">
+            <span>
+              {" "}
+              <img src={userIcon} alt="home" />
+            </span>
+            <span>New Listing</span>
+          </Link>
+        </li>
+        <li className="mb-2  w-full flex pl-16 py-2  hover:bg-[#D5E9FF] hover:text-black  text-lg  text-left">
+          <Link className="flex gap-x-4 items-center justify-center" to="">
+            <span>
+              {" "}
+              <img src={searchIcon} alt="home" />
+            </span>
+            <span>Search</span>
+          </Link>
+        </li>
+        <li className="mb-2  w-full flex pl-16 py-2  hover:bg-[#D5E9FF] hover:text-black  text-lg  text-left">
+          <Link className="flex gap-x-4 items-center justify-center" to="">
+            <span>
+              {" "}
+              <img src={aboutIcon} alt="home" />
+            </span>
+            <span>About</span>
+          </Link>
+        </li>
+        <li className="-ml-[2px]  w-full flex pl-16 py-2  hover:bg-[#D5E9FF] hover:text-black text-lg   text-left">
+          <Link className="flex gap-x-4 items-center justify-center" to="">
+            <span>
+              {" "}
+              <img src={favoriteIcon} alt="home" />
+            </span>
+            <span>Favorites</span>
+          </Link>
+        </li>
+        <hr className="my-5 w-4/5 ml-10" />
+        <li className="mb-2 -ml-[2px]  w-full flex pl-16 py-2  hover:bg-[#D5E9FF] hover:text-black text-lg   text-left">
+          <Link className="flex gap-x-4 items-center justify-center" to="">
+            <span>
+              {" "}
+              <img src={infoIcon} alt="home" />
+            </span>
+            <span>Help Center</span>
+          </Link>
+        </li>
+        <li className="mb-2 -ml-[2px]  w-full flex pl-16 py-2  hover:bg-[#D5E9FF] hover:text-black text-lg   text-left">
+          <Link className="flex gap-x-4 items-center justify-center" to="">
+            <span>
+              {" "}
+              <img src={settingsIcon} alt="home" />
+            </span>
+            <span>Settings</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 
-
 const Main = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div className="flex ">
+      <div className="w-1/4 ">
+        <Header></Header>
+      </div>
+      <div className="flex-1">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
