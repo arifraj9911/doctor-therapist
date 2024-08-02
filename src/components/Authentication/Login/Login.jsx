fb2eb4b8132e78255dba26cef4f97cfd1d3bf3f0
Arif Hossain <arif.raj9911@gmail.com>
2024-08-02 20:05:53 +0600

    login form design added

--- src/components/Authentication/Login/Login.jsx
+++ src/components/Authentication/Login/Login.jsx
@@ -1,28 +1,82 @@
 import { FcGoogle } from "react-icons/fc";
 import loginImage from "../../../assets/images/login.png";
 import { FaFacebook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="max-w-screen-xl mx-auto">
-      <div className="flex justify-between p-20">
-        <div>
+    <div className="max-w-screen-xl mx-auto ">
+      <div className="flex justify-between gap-20 p-20">
+        <div className="w-1/2">
           <h2 className="text-[40px] text-[#4285F3]">LOGO</h2>
-          <h4 className="text-3xl">Log In To Your Account</h4>
+          <h4 className="text-3xl my-3 font-bold">Log In To Your Account</h4>
           <p>Welcome Back! Select a method to log in:</p>
-          <div className="flex gap-6 items-center mt-6">
-            <div className="flex shadow-md rounded-lg items-center gap-2 bg-gradient-to-l  from-[#FFFFFF] to-[#E4E4E4] py-2 px-5">
+          <div className="flex gap-6  items-center mt-6">
+            <div className="flex shadow-md justify-center rounded-lg items-center gap-2 bg-gradient-to-l  from-[#FFFFFF] to-[#E4E4E4] w-full py-3 px-5">
               <FcGoogle />
               <button>Google</button>
             </div>
-            <div className="flex shadow-md rounded-lg items-center gap-2 bg-gradient-to-l text-white from-[#298FFF] to-[#0778F5] py-2 px-5">
+            <div className="flex w-full justify-center shadow-md rounded-lg items-center gap-2 bg-gradient-to-l text-white from-[#298FFF] to-[#0778F5] py-3 px-5">
               <FaFacebook />
               <button>Facebook</button>
             </div>
           </div>
+          <div className="divider my-8">or Continue with Email</div>
+          {/* login form */}
+          <form>
+            <div className="flex flex-col gap-1">
+              <label className="text-[16px] font-semibold" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                className="input input-bordered outline-none focus:outline-none btn-md"
+                name="email"
+                placeholder="Enter your email"
+                id=""
+              />
+            </div>
+            <div className="flex flex-col gap-1 mt-6">
+              <label className="text-[16px] font-semibold" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                className="input input-bordered outline-none focus:outline-none btn-md"
+                name="email"
+                placeholder="Enter your password"
+                id=""
+              />
+            </div>
+            <div className="flex justify-between mt-1">
+              <div className="space-x-2">
+                <input type="checkbox" name="" id="" />
+                <span className="text-sm">Remember Me</span>
+              </div>
+              <div>
+                <span className="text-[#156BCA] font-medium text-sm underline">
+                  Forgot Password
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-10 w-3/4 mx-auto">
+              <button className="px-4 py-2 w-full text-[16px] rounded-[10px] text-white bg-[#156BCA]">
+                Sing In
+              </button>
+            </div>
+          </form>
+          <div className="text-center mt-2">
+            <p className="text-sm">
+              Don&apos;t Have an account?{" "}
+              <span className="text-[#156BCA] underline">
+                <Link to="/register">Create Account</Link>
+              </span>{" "}
+            </p>
+          </div>
         </div>
-        <div className="w-1/2">
-          <img className="w-3/4 mx-auto " src={loginImage} alt="" />
+        <div className="w-4/5">
+          <img className="w-4/5 mx-auto " src={loginImage} alt="" />
         </div>
       </div>
     </div>
